ee4fb77bcb9ca420c3ba50e56d87fef9da2c28a0
Soonyoung Park <soon.syp@gmail.com>
2025-07-24 23:40:40 +0200

    seperate addtoCart alert

---
+++ src/components/AddToCartBox.tsx
@@ -0,0 +1,67 @@
+import type { ProductSortedProps } from '../types'
+import { getQntFromCartCookie } from '../utils/cookiesUtils'
+import { getQntFromUserCart } from '../utils/userUtils'
+import { useNavigate } from 'react-router-dom'
+import { useAuth } from '../context/auth'
+import { useState, useEffect } from 'react'
+
+function AddToCartBox({ product, sizeIndex, colorIndex, addtoCart }: {
+  product: ProductSortedProps,
+  sizeIndex: number | null,
+  colorIndex: number,
+  addtoCart: boolean
+}) {
+  const navigate = useNavigate()
+  const { user } = useAuth()
+
+  const [quantity, setQuantity] = useState<number>(0)
+
+  useEffect(() => {
+    const fetchQuantity = async () => {
+      if (!product || sizeIndex == null || colorIndex == null) return
+
+      const productKey = {
+        id: product.id,
+        size: sizeIndex,
+        color: colorIndex,
+      }
+
+      if (user) {
+        const res = await getQntFromUserCart(user.id, productKey)
+        if (res !== false && typeof res.data === 'number') {
+          setQuantity(res.data + 1)
+        } else {
+          setQuantity(1)
+        }
+      }
+      else {
+        const qnt = getQntFromCartCookie(productKey)
+        setQuantity(qnt !== undefined ? qnt : 1)
+      }
+    }
+
+    if (addtoCart) fetchQuantity()
+  }, [user, addtoCart])
+
+
+  return (
+    <div>
+      {addtoCart && <div className={`fixed inset-0 w-[100vw] h-[100vh] bg-[rgba(255,255,255,0)]`}></div>}
+      <div className={`cursor-pointer fixed top-0 right-0 w-[300px] bg-white grid grid-cols-[80px_1fr] p-4 shadow-xl gap-4 bg-gray-50 z-[999] duration-500
+        ${addtoCart ? '-translate-x-0' : 'translate-x-full'}
+        `}
+        onClick={() => navigate('/cart')}
+      >
+        <img className='rounded-md' src={product.images[colorIndex][0]} />
+        <div className='text-sm'>
+          <p>{product.name}</p>
+          <p><span className='mr-6'>Color</span>  {product.color[colorIndex]}</p>
+          <p><span className='mr-8'>Size</span>  {product.size[sizeIndex === null ? 0 : sizeIndex]}</p>
+          <p><span className='mr-2'>Qunatity</span>{quantity}</p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default AddToCartBox
--- src/components/AddToCartButton.tsx
+++ src/components/AddToCartButton.tsx
@@ -1,27 +1,45 @@
-import { removeFavorite, saveCart } from '../utils/cookieUtils'
+import type { ProductSortedProps } from '../types'
+import { removeCookie, saveCookie } from '../utils/cookiesUtils'
 import Button from './ui/button'
-import { useNavigate } from 'react-router-dom'
+import { useAuth } from '../context/auth'
+import { useLocation } from 'react-router-dom'
+import { addUserSelection, deleteUserSelection } from '../utils/userUtils'
 
-function AddButton({ id, sizeID, colorID, stock, setClicked }: {
-  id: number
-  sizeID: number | null,
-  colorID: number,
-  stock: number[][]
+function AddToCartButton({ product, sizeIndex, colorIndex, setAddtoCart, setClicked }: {
+  product: ProductSortedProps,
+  sizeIndex: number | null,
+  colorIndex: number,
+  setAddtoCart: React.Dispatch<React.SetStateAction<boolean>>
   setClicked?: React.Dispatch<React.SetStateAction<boolean>>
 }) {
-  const navigate = useNavigate()
+  const { user } = useAuth()
+  const pathname = useLocation().pathname
+
   const addtoCart = () => {
-    saveCart(id + '/' + sizeID + '/' + colorID +'-1')
-    removeFavorite(id + '/' + colorID)
-    if (setClicked) setClicked(false)
-    navigate('/cart')
+    if (user) {
+      addUserSelection('cart', user.id, { id: product.id, size: sizeIndex!, color: colorIndex, qnt: 1 })
+      deleteUserSelection('favorites', user.id, { id: product.id, size: 0, color: colorIndex })
+    } else {
+      saveCookie('cart', { id: product.id, size: sizeIndex!, color: colorIndex, qnt: 1 })
+      removeCookie('favorites', { id: product.id, size: 0, color: colorIndex })
+    }
+
+    setAddtoCart(true)
+    setClicked?.(false)
+
+    setTimeout(() => {
+      setAddtoCart(false)
+      if (pathname.includes('favorites')) window.location.reload()
+    }, 1500)
   }
 
   return (
-    <Button onClick={addtoCart} disabled={(sizeID === null) || (sizeID !== null && stock[sizeID][colorID] === 0)}>
-      {(sizeID !== null && stock[sizeID][colorID] === 0) ? 'Out of Stock' : 'Add'}
-    </Button>
+    <div>
+      <Button onClick={addtoCart} disabled={(sizeIndex === null) || (sizeIndex !== null && product.stock[sizeIndex][colorIndex] === 0)}>
+        {(sizeIndex !== null && product.stock[sizeIndex][colorIndex] === 0) ? 'Out of product.stock' : 'Add'}
+      </Button>
+    </div>
   )
 }
 
-export default AddButton
+export default AddToCartButton
