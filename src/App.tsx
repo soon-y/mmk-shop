b9119349286bd583b521959d22612692f737b645
Soonyoung Park <soon.syp@gmail.com>
2025-07-29 20:12:32 +0200

    add Cart & Favorites Provider, add checkout, order confirmation, order detail pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,48 @@
 import './App.css'
 import { Routes, Route } from 'react-router-dom'
+import { AuthProvider } from './context/auth'
+import { CartProvider } from './context/cart'
+import { FavoritesProvider } from './context/favorites'
 import Header from './components/Header'
 import Home from './pages/Home'
 import NotFound from './pages/NotFound'
-import Profile from './pages/Profile'
 import Items from './pages/Items'
 import ItemDetail from './pages/ItemDetail'
 import Footer from './components/footer'
 import Cart from './pages/Cart'
 import Favorites from './pages/Favorites'
-import { AuthProvider } from './context/auth'
 import Account from './pages/Account'
 import Orders from './pages/Orders'
 import Settings from './pages/Settings'
+import Checkout from './pages/Checkout'
+import OrderConfirmation from './pages/OrderConfirm'
+import OrderDetail from './pages/OrderDetail'
 
 function App() {
-
   return (
     <>
       <AuthProvider>
-        <Header />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/shopping/:categoryId" element={<Items />} />
-          <Route path="/products/:productId" element={<ItemDetail />} />
-          <Route path="/profile" element={<Profile />} />
-          <Route path="*" element={<NotFound />} />
-          <Route path="/cart" element={<Cart />} />
-          <Route path="/favorites" element={<Favorites />} />
-          <Route path="/account" element={<Account />}>
-            <Route path="orders" element={<Orders />} />
-            <Route path="settings" element={<Settings />} />
-          </Route>
-        </Routes>
-        <Footer />
+        <FavoritesProvider>
+          <CartProvider>
+            <Header />
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/shopping/:categoryId" element={<Items />} />
+              <Route path="/products/:productId" element={<ItemDetail />} />
+              <Route path="*" element={<NotFound />} />
+              <Route path="/cart" element={<Cart />} />
+              <Route path="/checkout" element={<Checkout />} />
+              <Route path="/favorites" element={<Favorites />} />
+              <Route path="/order-confirm" element={<OrderConfirmation />} />
+              <Route path="/account" element={<Account />}>
+                <Route path="orders" element={<Orders />} />
+                <Route path="orders/:orderId" element={< OrderDetail />} />
+                <Route path="settings" element={<Settings />} />
+              </Route>
+            </Routes>
+            <Footer />
+          </CartProvider>
+        </FavoritesProvider>
       </AuthProvider>
     </>
   )
