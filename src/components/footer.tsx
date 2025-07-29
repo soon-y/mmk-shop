c7446bbf1ac02a83005df127e5510192e8533f9b
Soonyoung Park <soon.syp@gmail.com>
2025-07-29 20:24:45 +0200

    disabled visibility to checkout page

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -5,11 +5,12 @@ import type { GroupedCategory } from '../types'
 import Dropdown from './ui/dropdown'
 import Logo from './ui/logo'
 import { FaFacebook, FaInstagram, FaLinkedinIn, FaXing, FaYoutube } from 'react-icons/fa'
-
+import { useLocation } from 'react-router-dom'
 
 function Footer() {
   const [category, setCategory] = useState<GroupedCategory[]>([])
   const navigate = useNavigate()
+  const pathname = useLocation().pathname
 
   useEffect(() => {
     fetchCategory().then((res) => {
@@ -17,102 +18,104 @@ function Footer() {
     })
   }, [])
 
-
-
   return (
-    <div className='p-4 md:p-6 flex flex-col gap-4'>
-      <div className='grid-cols-4 text-sm hidden md:grid'>
-        <div>
-          <div className='my-2 font-bold'>Shop</div>
-          <div className='flex flex-col gap-1 uppercase cursor-pointer'>
-            {category.map((el, i) => (
-              <div key={i}
-                onClick={() => navigate(`/shopping/product?group=${(el.name).toLocaleLowerCase()}&id=${el.id}`)}>
-                {el.name}
+    <>
+      {!pathname.includes('checkout') &&
+        <div className='p-4 md:p-6 flex flex-col gap-4'>
+          <div className='grid-cols-4 text-sm hidden md:grid'>
+            <div>
+              <div className='my-2 font-bold'>Shop</div>
+              <div className='flex flex-col gap-1 uppercase cursor-pointer'>
+                {category.map((el, i) => (
+                  <p key={i} className='hover:underline hover:text-magenta'
+                    onClick={() => navigate(`/shopping/product?group=${(el.name).toLocaleLowerCase()}&id=${el.id}`)}>
+                    {el.name}
+                  </p>
+                ))}
               </div>
-            ))}
-          </div>
-        </div>
+            </div>
 
-        <div>
-          <div className='my-2 font-bold'>Our Company</div>
-          <div className='flex flex-col gap-1 uppercase cursor-pointer'>
-            <p>About</p>
-            <p>press</p>
-            <p>Imprint</p>
-          </div>
-        </div>
-
-        <div>
-          <div className='my-2 font-bold'>Help</div>
-          <div className='flex flex-col gap-1 uppercase cursor-pointer'>
-            <p>customer service</p>
-            <p>my account</p>
-            <p>contact</p>
-            <p>gift cards</p>
-            <p>cookie notice</p>
-            <p>cookie settings</p>
-          </div>
-        </div>
-
-        <div>
-          <div className='my-2 font-bold'>Register Now</div>
-          <div className='flex flex-col gap-1'>
-            <p>Become a member to receive fantastic offers!</p>
-            <p className='uppercase underline'>Register</p>
-          </div>
-        </div>
-      </div>
-
-      <div className='md:hidden flex flex-col gap-4'>
-        <Dropdown title='shop'>
-          <div className='flex flex-col gap-1 cursor-pointer'>
-            {category.map((el, i) => (
-              <div key={i}
-                onClick={() => navigate(`/shopping/product?group=${(el.name).toLocaleLowerCase()}&id=${el.id}`)}>
-                {el.name}
+            <div>
+              <div className='my-2 font-bold'>Our Company</div>
+              <div className='flex flex-col gap-1 uppercase cursor-pointer'>
+                <p className='hover:underline hover:text-magenta'>About</p>
+                <p className='hover:underline hover:text-magenta'>press</p>
+                <p className='hover:underline hover:text-magenta'>Imprint</p>
               </div>
-            ))}
+            </div>
+
+            <div>
+              <div className='my-2 font-bold'>Help</div>
+              <div className='flex flex-col gap-1 uppercase cursor-pointer'>
+                <p className='hover:underline hover:text-magenta'>customer service</p>
+                <p className='hover:underline hover:text-magenta'>my account</p>
+                <p className='hover:underline hover:text-magenta'>contact</p>
+                <p className='hover:underline hover:text-magenta'>gift cards</p>
+                <p className='hover:underline hover:text-magenta'>cookie notice</p>
+                <p className='hover:underline hover:text-magenta'>cookie settings</p>
+              </div>
+            </div>
+
+            <div>
+              <div className='my-2 font-bold'>Register Now</div>
+              <div className='flex flex-col gap-1'>
+                <p>Become a member to receive fantastic offers!</p>
+                <p className='cursor-pointer uppercase underline hover:text-magenta'>Register</p>
+              </div>
+            </div>
           </div>
-        </Dropdown>
 
-        <Dropdown title='our company'>
-          <p>About</p>
-          <p>Press</p>
-          <p>Imprint</p>
-        </Dropdown>
+          <div className='md:hidden flex flex-col gap-4'>
+            <Dropdown title='shop'>
+              <div className='flex flex-col gap-1 cursor-pointer'>
+                {category.map((el, i) => (
+                  <div key={i}
+                    onClick={() => navigate(`/shopping/product?group=${(el.name).toLocaleLowerCase()}&id=${el.id}`)}>
+                    {el.name}
+                  </div>
+                ))}
+              </div>
+            </Dropdown>
 
-        <Dropdown title='help'>
-          <p>Customer service</p>
-          <p>My account</p>
-          <p>Contact</p>
-          <p>Gift cards</p>
-          <p>Cookie notice</p>
-          <p>Cookie settings</p>
-        </Dropdown>
+            <Dropdown title='our company'>
+              <p>About</p>
+              <p>Press</p>
+              <p>Imprint</p>
+            </Dropdown>
 
-        <Dropdown title='Register now'>
-          <p>Become a member to receive fantastic offers!</p>
-          <p className='uppercase underline'>Register</p>
+            <Dropdown title='help'>
+              <p>Customer service</p>
+              <p>My account</p>
+              <p>Contact</p>
+              <p>Gift cards</p>
+              <p>Cookie notice</p>
+              <p>Cookie settings</p>
+            </Dropdown>
 
-        </Dropdown>
-      </div>
+            <Dropdown title='Register now'>
+              <p>Become a member to receive fantastic offers!</p>
+              <p className='uppercase underline'>Register</p>
 
-      <div>
-        <p className='font-semibold'>Germany(€)</p>
-      </div>
+            </Dropdown>
+          </div>
 
-      <div className='mt-4 flex justify-between items-center inset-0 relative'>
-        <Logo classname='grayscale' />
-        <div className='flex items-center gap-4 md:gap-6'>
-          <FaFacebook />
-          <FaYoutube />
-          <FaXing />
-          <FaInstagram />
-          <FaLinkedinIn />
+          <div>
+            <p className='font-semibold'>Germany(€)</p>
+          </div>
+
+          <div className='mt-4 flex justify-between items-center inset-0 relative'>
+            <Logo classname='grayscale' />
+            <div className='flex items-center gap-4 md:gap-6'>
+              <FaFacebook />
+              <FaYoutube />
+              <FaXing />
+              <FaInstagram />
+              <FaLinkedinIn />
+            </div>
+          </div>
         </div>
-      </div>
-    </div>
+      }
+    </>
   )
 }
 
