c06b283400704349dcd6861f4ef2036f3fa5c6ad
Soonyoung Park <soon.syp@gmail.com>
2025-07-29 20:13:58 +0200

    add

---
+++ src/asset/Master.tsx
@@ -0,0 +1,41 @@
+const Master = () => {
+  return (
+    <svg
+      aria-label="Master card"
+      role="logo"
+      viewBox="0 0 131.39 86.9"
+      width="40px"
+      height="40px"
+    >
+
+      <g style={{ opacity: 0 }}>
+        <rect style={{ fill: '#fff' }} width="131.39" height="86.9" />
+      </g>
+
+      <rect
+        x="48.37"
+        y="15.14"
+        width="34.66"
+        height="56.61"
+        style={{ fill: '#ff5f00' }}
+      />
+
+      <path
+        d="M51.94,43.45a35.94,35.94,0,0,1,13.75-28.3,36,36,0,1,0,0,56.61A35.94,35.94,0,0,1,51.94,43.45Z"
+        style={{ fill: '#eb001b' }}
+      />
+
+      <path
+        d="M120.5,65.76V64.6H121v-.24h-1.19v.24h.47v1.16Zm2.31,0v-1.4h-.36l-.42,1-.42-1h-.36v1.4h.26V64.7l.39.91h.27l.39-.91v1.06Z"
+        style={{ fill: '#f79e1b' }}
+      />
+
+      <path
+        d="M123.94,43.45a36,36,0,0,1-58.25,28.3,36,36,0,0,0,0-56.61,36,36,0,0,1,58.25,28.3Z"
+        style={{ fill: '#f79e1b' }}
+      />
+    </svg>
+  )
+}
+
+export default Master
