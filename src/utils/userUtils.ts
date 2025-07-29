5b27c102ce5c3fffa37df89dc18e20b8ec45f882
Soonyoung Park <soon.syp@gmail.com>
2025-07-29 20:30:39 +0200

    add functions for order

--- src/utils/userUtils.ts
+++ src/utils/userUtils.ts
@@ -1,7 +1,7 @@
 import axios, { type AxiosResponse } from 'axios'
-import type { UserSelectionProps } from '../types'
+import type { OrderedProductProps, OrderProps, UserSelectionProps } from '../types'
 
-export const fetchUserSelection = async (name: string, user: string): Promise<AxiosResponse<UserSelectionProps[]> | false> => {
+export const fetchUserSelection = async (name: string, user: string): Promise<UserSelectionProps[] | false> => {
   try {
     const res = await axios.get(`https://mmk-backend.onrender.com/${name}`, {
       params: { user },
@@ -24,8 +24,8 @@ export const addUserSelection = async (name: string, user: string, product: User
 }
 
 export const deleteUserSelection = async (name: string, user: string, product: UserSelectionProps): Promise<AxiosResponse | false> => {
+  
   try {
-    console.log(product)
     const res = axios.post(`https://mmk-backend.onrender.com/${name}/delete`, {
       user, product
     })
@@ -46,12 +46,65 @@ export const updateCartQnt = async (user: string, index: number, qnt: number): P
   }
 }
 
+export const dropUserCart = async (user: string): Promise<AxiosResponse | false> => {
+  try {
+    const res = axios.post(`https://mmk-backend.onrender.com/cart/drop`, {
+      user
+    })
+    return res
+  } catch (err) {
+    return false
+  }
+}
+
+export const addOrder = async (orderInfo: OrderProps, productInfo: OrderedProductProps[]): Promise<AxiosResponse | false> => {
+  try {
+    const res = axios.post(`https://mmk-backend.onrender.com/orders/add`, {
+      orderInfo, productInfo
+    })
+    return res
+  } catch (err) {
+    return false
+  }
+}
+
+export const fetchOrder = async (user: string): Promise<OrderProps | null> => {
+  try {
+    const res = await axios.get(`https://mmk-backend.onrender.com/orders`, {
+      params: { user }
+    })
+
+    if (res.status === 200 || res.status === 201) {
+      return res.data
+    } else {
+      return null;
+    }
+  } catch (err) {
+    return null;
+  }
+}
+
+
 export const getQntFromUserCart = async (user: string, product: UserSelectionProps): Promise<AxiosResponse<number> | false> => {
   try {
     const res = axios.get(`https://mmk-backend.onrender.com/cart/getQnt`, {
       params: { user, id: product.id, size: product.size, color: product.color, },
     })
     return res
+  } catch (err) {
+    return false
+  }
+}
+
+export const getTotalQntFromUserCart = async (user: string): Promise<number | false> => {
+  try {
+    const res = await axios.get(`https://mmk-backend.onrender.com/cart`, {
+      params: { user },
+    })
+
+    const total: number = res.data.reduce((sum: number, el: UserSelectionProps) => sum + (el.qnt ?? 0), 0)
+
+    return total
   } catch (err) {
     return false
   }
