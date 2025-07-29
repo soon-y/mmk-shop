a19be87c6d6f455cb484fcadf89747ffb31f3a11
Soonyoung Park <soon.syp@gmail.com>
2025-07-29 20:29:13 +0200

    add

---
+++ src/components/cart/orderValue.tsx
@@ -0,0 +1,73 @@
+import { useState, useEffect } from 'react'
+import type { UserSelectionProps, ProductSortedProps } from '../../types'
+import { CircleQuestionMark } from 'lucide-react'
+import { useCart } from '../../context/cart'
+
+function OrderValue() {
+  const [productsInCart, setProductsInCart] = useState<ProductSortedProps[]>([])
+  const [userCart, setUserCart] = useState<UserSelectionProps[]>([])
+  const [total, setTotal] = useState<number>(0)
+  const [deliveryCharge, setDeliveryCharge] = useState<number>(0)
+  const { products, userSelection, loading } = useCart()
+
+  useEffect(() => {
+    setProductsInCart(products)
+    setUserCart(userSelection)
+  }, [products, userSelection])
+
+  useEffect(() => {
+    let total = 0
+
+    userCart.forEach((el, i) => {
+      const quantity = el.qnt ?? 1
+      total += quantity * productsInCart[i].price
+    })
+
+    const deliveryCharge = total >= 50 ? 0 : 1.49
+    setTotal(total)
+    setDeliveryCharge(deliveryCharge)
+  }, [userCart])
+
+  return (
+    <div>
+      {loading ?
+        <div className='animate-pulse flex flex-col gap-2'>
+          <p className='bg-gray-100 h-5 w-full rounded-sm'></p>
+          <p className='bg-gray-100 h-5 w-full rounded-sm'></p>
+          <p className='bg-gray-100 h-5 w-full rounded-sm'></p>
+          <p className='bg-gray-100 h-5 w-full rounded-sm mt-2 mb-1'></p>
+        </div>
+        :
+        <div>
+          <div>
+            <div className='flex justify-between'>
+              <p>Order value</p>
+              <p className='font-bold'>{total.toFixed(2)} €</p>
+            </div>
+            <div className='flex justify-between'>
+              <p>Discount</p>
+              <p className='font-bold text-red-500'>-0 €</p>
+            </div>
+            <div className='flex justify-between mb-2'>
+              <div>
+                <p>Estimated delivery charge</p>
+                {deliveryCharge !== 0 &&
+                  <p className='text-gray-400 text-xs'>
+                    <CircleQuestionMark className='inline-block w-4' /> Free for orders over €30
+                  </p>}
+              </div>
+              <p className='font-bold'>{deliveryCharge} €</p>
+            </div>
+          </div>
+
+          <div className='flex justify-between my-4'>
+            <p>TOTAL</p>
+            <p className='font-bold'>{productsInCart.length > 0 && (total + deliveryCharge).toFixed(2)} €</p>
+          </div>
+        </div>
+      }
+    </div>
+  )
+}
+
+export default OrderValue
---
+++ src/pages/Checkout.tsx
@@ -0,0 +1,260 @@
+import { useState, useEffect, useRef } from 'react'
+import Button from '../components/ui/button'
+import type { UserSelectionProps, ProductSortedProps } from '../types'
+import { useNavigate } from 'react-router-dom'
+import { ArrowLeft, Trash2, LockKeyhole, Loader2 } from 'lucide-react'
+import OrderValue from '../components/cart/orderValue'
+import { useCart } from '../context/cart'
+import { useAuth } from '../context/auth'
+import MyInfo from '../components/user/myInfo'
+import MyAddr from '../components/user/myAddr'
+import MyBillingAddr from '../components/user/myBillingAddr'
+import MyPayment from '../components/user/myPayment'
+import RightSidePanel from '../components/RightSidePanel'
+import { addOrder } from '../utils/userUtils'
+
+function Checkout() {
+  const navigate = useNavigate()
+  const [productsInCart, setProductsInCart] = useState<ProductSortedProps[]>([])
+  const [userCart, setUserCart] = useState<UserSelectionProps[]>([])
+  const [openPackage, setOpenPackage] = useState<boolean>(false)
+  const { products, userSelection, loading, deleteProduct, totalQnt, dropCart } = useCart()
+  const [deliveryCharge, setDeliveryCharge] = useState<number>(0)
+  const { user, addr, billingAddr } = useAuth()
+  const [addrIndex, setAddrIndex] = useState<number>(-1)
+  const [billingAddrIndex, setBillingAddrIndex] = useState<number>(-1)
+  const [waiting, setWaiting] = useState<boolean>(false)
+  const [openShippingInfo, setOpenShippingInfo] = useState<boolean>(false)
+  const packageRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    setProductsInCart(products)
+    setUserCart(userSelection)
+    packageRef.current?.scrollIntoView({ behavior: 'smooth' })
+    console.log(userSelection)
+  }, [products, userSelection])
+
+  useEffect(() => {
+    let total = 0
+
+    userCart.forEach((el, i) => {
+      const quantity = el.qnt ?? 1
+      total += quantity * productsInCart[i].price
+    })
+    const deliveryCharge = total >= 50 ? 0 : 1.49
+    setDeliveryCharge(deliveryCharge)
+  }, [userCart])
+
+  useEffect(() => {
+    if (totalQnt === 0) navigate('/cart')
+  }, [totalQnt])
+
+  useEffect(() => {
+    if (addr) {
+      setAddrIndex(addr?.findIndex((el) => el.select === true))
+    }
+  }, [addr])
+
+  useEffect(() => {
+    if (billingAddr) setBillingAddrIndex(billingAddr?.findIndex((el) => el.select === true))
+  }, [billingAddr])
+
+  return (
+    <div>
+      <div className='p-4 md:p-6'>
+        <p className='mb-4 cursor-pointer underline flex hover:text-magenta' onClick={() => navigate('/cart')}>
+          <ArrowLeft className='button' /> Back to cart
+        </p>
+        <h1>Checkout</h1>
+        <div className='gap-8 grid grid-rows md:grid-cols-[1fr_300px] lg:grid-cols-[1fr_400px]'>
+          <div className='flex flex-col gap-4'>
+            <div>
+              <h3>My information</h3>
+              <MyInfo />
+            </div>
+
+            <hr />
+
+            <div>
+              <h3>billing address</h3>
+              <MyBillingAddr />
+            </div>
+
+            <hr />
+
+            <div>
+              <h3>delivery address</h3>
+              <MyAddr />
+            </div>
+
+            <hr />
+
+            <div ref={packageRef}>
+              <h3>Package</h3>
+              <p>Shipped by MMK</p>
+              <p id='package' className='uppercase py-2 underline cursor-pointer' onClick={() => setOpenPackage(true)}>{totalQnt > 1 ? totalQnt + ' articles' : '1 article'}</p>
+              {!loading ?
+                <div className='w-full grid grid-cols-4 gap-2 items-center'>
+                  {productsInCart.map((item, i) => (
+                    <div className='relative' key={i}>
+                      <img className='rounded-md' src={item.images[userCart[i].color][0]} />
+                      <Trash2 className='absolute bottom-0 right-0 bg-white/80 p-2 w-10 h-10 cursor-pointer' onClick={() => {
+                        deleteProduct(i)
+                      }} />
+                    </div>
+                  ))}
+                </div>
+                :
+                <div className='w-full grid grid-cols-4 gap-2 items-center'>
+                  {Array.from({ length: 4 }).map((_, i) => (
+                    <div key={i} className='w-full aspect-square rounded-md bg-gray-100'></div>
+                  ))}
+                </div>
+              }
+
+              <p className='mt-8 font-bold'>Standard delivery. Free for orders over €50</p>
+              <p>{deliveryCharge === 0 ? 'For free. 2-4 working days' : '€' + deliveryCharge}</p>
+            </div>
+
+            <hr />
+
+            <div>
+              <h3>checkout payment</h3>
+              <MyPayment />
+            </div>
+          </div>
+
+          <div className='flex flex-col gap-4 mt-4 md:mt-0 sticky top-6 self-start'>
+            <OrderValue />
+            <p className='text-sm'>We process your personal data in accordance with <span className='underline'>MMK's privacy policy</span>.</p>
+            <p className='text-sm mb-4'>By continuing, you agree to  <span className='underline'>MMK's Terms and Conditions</span>.</p>
+            <Button disabled={
+              !user || !addr || !billingAddr || addrIndex === -1 || billingAddrIndex === -1
+              || !user?.contact || !user.payment
+              || !addr[addrIndex].contact || !billingAddr[billingAddrIndex].contact
+              || !addr[addrIndex].street || !billingAddr[billingAddrIndex].street
+            } onClick={() => {
+              setWaiting(true)
+
+              let total: number = 0
+              userCart.forEach((el, i) => {
+                const quantity = el.qnt ?? 1
+                total += quantity * productsInCart[i].price
+              })
+
+              addOrder({
+                userId: user!.id,
+                status: 'ordered',
+                totalAmount: total,
+                discount: 0,
+                shippingFee: deliveryCharge,
+                paidAmount: total + deliveryCharge,
+                paymentMethod: user!.payment!,
+                paymentStatus: 'processed',
+                transactionId: '',
+                shippingAddr: addr![addrIndex].street + ' ' + addr![addrIndex].postalCode + ' ' + addr![addrIndex].city + ' ' + addr![addrIndex].country,
+                billingAddr: billingAddr![billingAddrIndex].street + ' ' + billingAddr![billingAddrIndex].postalCode + ' ' + billingAddr![billingAddrIndex].city + ' ' + billingAddr![billingAddrIndex].country,
+                shippingName: addr![addrIndex].firstName + ' ' + addr![addrIndex].lastName,
+                billingName: billingAddr![billingAddrIndex].firstName + ' ' + billingAddr![billingAddrIndex].lastName,
+                shippingContact: addr![addrIndex].contact,
+                billingContact: billingAddr![billingAddrIndex].contact,
+              },
+                userSelection.map((el, i) => ({
+                  userId: user!.id,
+                  productId: el.id!,
+                  size: products[i].size[el.size],
+                  color: products[i].color[el.color],
+                  quantity: el.qnt!,
+                  total: Number(el.qnt!) * Number(products[i].price)
+                })))
+
+              setTimeout(() => {
+                dropCart()
+                navigate('/order-confirm')
+                setWaiting(false)
+              }, 3000)
+            }
+            }>{waiting ?
+              <Loader2 className='w-5 h-5 animate-spin' /> :
+              'complete purchase'
+              }</Button>
+            <p className='text-sm mt-4 flex items-center'>
+              <LockKeyhole className='w-4 mr-2' />
+              All data is stored securely. Payment details are encrypted.
+            </p>
+            <p className='text-sm'>Do you need help? Please contact <span className='underline cursor-pointer' onClick={() => navigate('/customer-service')}>customer service</span>.</p>
+            <p className='uppercase underline mt-4 cursor-pointer' onClick={() => setOpenShippingInfo(true)}>Shipping and return options</p>
+          </div>
+        </div>
+      </div >
+
+      <RightSidePanel clicked={openPackage} setClicked={setOpenPackage} title='view oerder details'>
+        {openPackage &&
+          <div className='p-6 flex flex-col gap-4'>
+            <div>
+              <p className=''>Package</p>
+              <p className=''>Shipped by MMK</p>
+            </div>
+
+            {productsInCart && productsInCart.map((item, i) => (
+              <div className='grid grid-cols-[180px_1fr] gap-4' key={i}>
+                <div className='relative'>
+                  <img className='rounded-md' src={item.images[userCart[i].color][0]} />
+                  <Trash2 className='absolute bottom-0 right-0 bg-white/80 p-2 w-10 h-10 cursor-pointer' onClick={() => {
+                    deleteProduct(i)
+                  }} />
+                </div>
+
+                <div className='h-full flex flex-col gap-1 text-sm'>
+                  <p>{item.name}</p>
+                  <p className='font-bold'>€ {item.price}</p>
+                  <div>
+                    <p className="flex gap-1 mt-4">
+                      Size: <span>{item.size[userSelection[i].size]}</span>
+                    </p>
+                    <p className="flex gap-1">
+                      Color: <span>{item.color[userSelection[i].color]}</span>
+                    </p>
+                    <p className="flex gap-1">
+                      Quantity: <span>{userSelection[i].qnt}</span>
+                    </p>
+                    <p className="flex gap-1">
+                      Total: <span className='font-bold'>€ {Number(userSelection[i].qnt) * item.price}</span>
+                    </p>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>}
+      </RightSidePanel>
+
+      <RightSidePanel title='Shipping and return options' clicked={openShippingInfo} setClicked={setOpenShippingInfo}>
+        <div className='p-6 flex flex-col gap-2'>
+          <p className='uppercase'>Shipment</p>
+          <ul className='text-sm'>
+            <li>
+              Standard shipping: €1.49, free for orders over €50
+            </li>
+            <li>
+              Delivery within 2-4 business days.
+            </li>
+            <li>
+              The current delivery time for your order will be displayed at checkout and on your order confirmation.
+            </li>
+          </ul>
+
+          <p className='uppercase'>Return</p>
+          <ul className='text-sm'>
+            <li>You can return your order within 30 days. Sale items can be returned within 14 days of receiving the order.</li>
+            <li>The return fee is €2.99 per order</li>
+          </ul>
+
+          <p className='uppercase'>You can find more information on our customer service page under Delivery or Returns.</p>
+        </div>
+      </RightSidePanel>
+
+    </div>
+  )
+}
+
+export default Checkout
