cdf6dc8cf53252fcc6ca8aaf06072b10622f8211
Soonyoung Park <soon.syp@gmail.com>
2025-07-29 20:26:29 +0200

    display horizontally and enable scroll-x

--- src/components/RecentView.tsx
+++ src/components/RecentView.tsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react'
 import type { UserSelectionProps, ProductSortedProps } from '../types'
 import { getCookiesProducts } from '../utils/productUtils'
-import ItemBox from '../components/ItemBox'
+import ItemBox from './box/ItemBox'
 
 function RecentView() {
   const [productsInHistory, setProductsInHistory] = useState<ProductSortedProps[]>([])
@@ -18,12 +18,10 @@ function RecentView() {
     <div>
       {productsInHistory.length > 0 &&
         <div>
-          <div className='uppercase pt-12 mb-2'>Recently viewed</div>
-          <div className='grid grid-cols-3 md:grid-cols-4 lg:grid-cols-6 gap-x-2 gap-y-4 flex-wrap'>
-            {productsInHistory.slice(0, 12).map((item, i) => (
-              <div key={i}>
-                <ItemBox product={item} color={userHistory[i].color} />
-              </div>
+          <div className='uppercase pt-12 px-4 md:px-6 pb-2'>Recently viewed</div>
+          <div className='flex overflow-x-auto flex-nowrap hide-scrollbar gap-2 px-4 md:px-6'>
+            {productsInHistory.map((item, i) => (
+              <ItemBox product={item} color={userHistory[i].color} classname='' key={i} />
             ))}
           </div>
         </div>
